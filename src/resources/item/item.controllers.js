f87e1d60610bffa50187c833773b4d23da09db19
AgustinRow <agus.diaz.11@gmail.com>
2021-11-11 14:16:20 -0300

    lesson 3

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,28 +1,4 @@
 import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
-import mongoose from 'mongoose'
-import { connect } from '../../utils/db'
-
-const run = async () => {
-  try {
-    await connect('mongodb://localhost:27017/api-test')
-    const item = await Item.create({
-      name: 'Clean',
-      createdBy: mongoose.Types.ObjectId(),
-      list: mongoose.Types.ObjectId()
-    })
-
-    const updated = await await Item.findByIdAndUpdate(
-      item._id,
-      { name: 'Eat' },
-      { new: true }
-    ).exec()
-
-    console.log(updated)
-  } catch (error) {
-    console.log(error)
-  }
-}
-run()
 
 export default crudControllers(Item)
