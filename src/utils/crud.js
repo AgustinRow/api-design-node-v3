913ca67b30abd09fd811a3d3f22d85367cc2ce48
AgustinRow <agus.diaz.11@gmail.com>
2021-11-11 15:52:18 -0300

    all finished

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,14 +1,54 @@
 export const getOne = model => async (req, res) => {
-  console.log('nothing')
+  const { id } = req.params
+  const userId = req.user._id
+
+  const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
+  if (!doc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: doc })
 }
 
-export const getMany = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const userId = req.user
+  const doc = await model.find({ createdBy: userId })
+  res.status(200).json({ data: doc })
+}
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  const userId = req.user
+  const { name } = req.body
 
-export const updateOne = model => async (req, res) => {}
+  const doc = await model.create({ name: name, createdBy: userId })
+  res.status(201).json({ data: doc })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  const userId = req.user._id
+  const listId = req.params.id
+  const { body } = req
+
+  const doc = await model
+    .findOneAndUpdate({ _id: listId, createdBy: userId }, body, { new: true })
+    .exec()
+  if (!doc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: doc })
+}
+
+export const removeOne = model => async (req, res) => {
+  const doc = await model
+    .findOneAndRemove({ _id: req.params.id, createdBy: req.user._id })
+    .exec()
+  if (!doc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
